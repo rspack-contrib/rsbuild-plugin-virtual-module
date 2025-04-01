2b2ef2553ecfed5c264af11bdc597507cf86ac18
SoonIter <sooniter@gmail.com>
2025-04-01 21:41:38 +0800

    fix: add the test

--- test/basic/index.test.ts
+++ test/basic/index.test.ts
@@ -1,8 +1,9 @@
-import { dirname } from 'node:path';
+import { readFile, readdir } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { expect, test } from '@playwright/test';
 import { createRsbuild } from '@rsbuild/core';
-import { pluginVirtualModule } from '../../src';
+import { pluginVirtualModule } from 'rsbuild-plugin-virtual-module';
 import { getRandomPort } from '../helper';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -11,7 +12,15 @@ test('should render page as expected', async ({ page }) => {
   const rsbuild = await createRsbuild({
     cwd: __dirname,
     rsbuildConfig: {
-      plugins: [pluginVirtualModule()],
+      plugins: [
+        pluginVirtualModule({
+          virtualModules: {
+            'virtual-json-list': async () => {
+              return 'export default {}';
+            },
+          },
+        }),
+      ],
       server: {
         port: getRandomPort(),
       },
@@ -21,7 +30,7 @@ test('should render page as expected', async ({ page }) => {
   const { server, urls } = await rsbuild.startDevServer();
 
   await page.goto(urls[0]);
-  expect(await page.evaluate('window.test')).toMatchSnapshot();
+  expect(JSON.stringify(await page.evaluate('window.test'))).toMatchSnapshot();
 
   await server.close();
 });
@@ -29,13 +38,36 @@ test('should render page as expected', async ({ page }) => {
 test('should build succeed', async ({ page }) => {
   const rsbuild = await createRsbuild({
     cwd: __dirname,
+    rsbuildConfig: {
+      plugins: [
+        pluginVirtualModule({
+          virtualModules: {
+            'virtual-json-list': async ({ addDependency }) => {
+              const jsonFolderPath = join(__dirname, 'json');
+              const ls = await readdir(jsonFolderPath);
+              const res: Record<string, unknown> = {};
+              for (const file of ls) {
+                if (file.endsWith('.json')) {
+                  const jsonFilePath = join(jsonFolderPath, file);
+                  const jsonContent = await readFile(jsonFilePath, 'utf-8');
+                  addDependency(jsonFilePath);
+                  res[file] = JSON.parse(jsonContent);
+                }
+              }
+
+              return `export default ${JSON.stringify(res)};`;
+            },
+          },
+        }),
+      ],
+    },
   });
 
   await rsbuild.build();
   const { server, urls } = await rsbuild.preview();
 
   await page.goto(urls[0]);
-  expect(await page.evaluate('window.test')).toMatchSnapshot();
+  expect(JSON.stringify(await page.evaluate('window.test'))).toMatchSnapshot();
 
   await server.close();
 });
